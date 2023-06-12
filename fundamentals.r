6380ea725bf9c4b3e10f067575024022d68cee0a
Guilherme Soares Garcia <guilherme.garcia@email.com>
2023-06-12 16:16:41 -0300

    Testando funcionamento de vetores, listas e algumas funçoes auxiliares

--- fundamentals.r
+++ fundamentals.r
@@ -2,5 +2,23 @@
 first_variable <- "This is my variable"
 second_variable <- 12.5
 
-#Vetores (lista)
+#Vetores
 vec_1 <- c(13, 48.5, 71, 101.5, 2)
+names(vec_1) <- c("a","b","c")
+
+print(vec_1)
+print(length(vec_1))
+print(typeof(c(1L, 5L)))
+print(typeof(vec_1))
+
+#Listas
+list_1 <- list("a", 1L, 1.5, TRUE)
+print(list_1)
+
+str(list_l)
+
+x <- list('Chicago' = 1, 'New York' = 2, 'Los Angeles' = 3)
+
+print(x[2])
+print(x[3])
+
