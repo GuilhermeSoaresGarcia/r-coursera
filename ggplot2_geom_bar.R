c489fa6d9e13ecb6578c2b527f22af3402ea65ad
Guilherme Soares Garcia <guilherme.garcia@email.com>
2023-06-20 12:24:41 -0300

    Utilizando geom_bar() em ggplot2

--- ggplot2_geom_bar.R
+++ ggplot2_geom_bar.R
@@ -5,8 +5,16 @@ data("diamonds")
 ggplot(data = diamonds,
        mapping = aes(
          x = cut,
-         fill = cut)
+         fill = cut
+         )
        ) +
 # A função "geom_bar()" utiliza apenas o eixo X pois o Y é definido automaticamente
 # através do número de linhas existente na variável utilizada na plotagem
-geom_bar()
+geom_bar() +
+  labs(
+    title = 'Quantidade de diamantes por tipo de corte',
+    x = "Tipo de corte",
+    y = "Quantidade"
+    ) +
+guides(fill = "none")
+
